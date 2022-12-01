0b2550995d6efbe9538c6c5c9dd4ebc8e12491e2
matthew butler <virtualprodigyllc@gmail.com>
2022-12-01 13:05:50 -0500

    Day 1 results

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,51 @@
+import kotlin.math.max
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var max = 0
+        var temp = 0
+        input.forEachIndexed { index, str ->
+            if (str.isNotEmpty()) {
+                temp += str.toInt()
+                if (index == input.size - 1) {
+                    max = max(max, temp)
+                }
+            } else {
+                max = max(max, temp)
+                temp = 0
+            }
+
+        }
+        return max
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+
+        val totals = mutableListOf<Int>()
+        var temp = 0
+        input.forEachIndexed { index, str ->
+            if (str.isNotEmpty()) {
+                temp += str.toInt()
+                if (index == input.size - 1) {
+                    totals.add(temp)
+                }
+            } else {
+                totals.add(temp)
+                temp = 0
+            }
+
+        }
+        totals.sortDescending()
+
+        val sum = totals[0] + totals[1] + totals[2]
+//        print("sum $sum  collection $totals")
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
